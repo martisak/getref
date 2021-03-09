8f409fac6045a9e11b51ba910279691bf0e48da2
Martin Isaksson <martin.isaksson@ericsson.com>
2021-03-09 18:22:42 +0100

    Add trove classifiers

--- setup.py
+++ setup.py
@@ -8,6 +8,14 @@ setup(
     author_email='martin.isaksson@gmail.com',
     keywords='bibtex dblp',
     license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'Environment :: Console'
+      ],
     packages=find_packages(),
     install_requires=[
           'simple-term-menu==0.10.5',
